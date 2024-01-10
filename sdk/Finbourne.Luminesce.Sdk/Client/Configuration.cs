a76eba0505df01563fa2398c590ca5841475c806
Concourse <concourse@finbourne.com>
2024-01-10 17:00:19 +0000

    Updates for v2.0.55

--- sdk/Finbourne.Luminesce.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Luminesce.Sdk/Client/Configuration.cs
@@ -543,7 +543,7 @@ namespace Finbourne.Luminesce.Sdk.Client
             string report = "C# SDK (Finbourne.Luminesce.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 1.14.286\n";
+            report += "    Version of the API: 1.14.296\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
