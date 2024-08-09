9055950a08397ea595c206043f464bacf00c09bf
Concourse <concourse@finbourne.com>
2024-08-09 11:04:13 +0000

    luminesce csharp updates for v2.1.100
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/Finbourne.Luminesce.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Luminesce.Sdk/Client/Configuration.cs
@@ -544,7 +544,7 @@ namespace Finbourne.Luminesce.Sdk.Client
             string report = "C# SDK (Finbourne.Luminesce.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 1.16.423\n";
+            report += "    Version of the API: 1.16.430\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
